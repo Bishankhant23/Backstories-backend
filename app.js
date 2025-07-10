3c499c8f4eb196f307d6cb95e671086d620988a8
bishanpatel123 <bishanpatel123@gmail.com>
2025-07-11 01:22:25 +0530

    Changed cors origin

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ app.use(cookieParser());
 app.use(morgan("dev"));
 
 app.use(cors({
-  origin : "http://localhost:5173",
+  origin : ["http://localhost:5173","https://backstories-cxqf.vercel.app"],
   credentials : true
 }))
 
